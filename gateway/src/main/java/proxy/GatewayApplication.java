70564d38dc2df8f1ed7009741b68dfa41c1d67ac
Chris Strahl <christiandstrahl@gmail.com>
2019-10-13 14:08:28 -0400

    Set up gateway service for routing api calls from front-end to back-end

---
+++ gateway/src/main/java/proxy/GatewayApplication.java
@@ -0,0 +1,21 @@
+package proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import proxy.filters.LoggingFilter;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public LoggingFilter loggingFilter(){
+        return new LoggingFilter();
+    }
+}
